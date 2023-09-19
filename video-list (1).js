14e0c0150fa8931a8e6e6761f0f3369c48f52d5a
Claytonmodderr <121697788+Claytonmodderr@users.noreply.github.com>
2023-09-19 01:24:21 -0300

    Add files via upload

---
+++ video-list (1).js
@@ -0,0 +1,203 @@
+let allVideos = [
+   {
+      name: "Sinal 1",
+      src: " https://tv.wcli.li/record/live.php?ch=0",
+      id: "vid_1"
+   },
+   {
+      name: "Sinal 1 ALT",
+      src: " https://tv.wcli.li/record/alt.php?ch=0",
+      id: "vid_2"
+   },
+   {
+      name: "Sinal 2",
+      src: "h#dOvLae3VlAk",
+      id: "vid_3"
+   },
+   {
+      name: "Sinal 2 ALT",
+      src: "h#li7NA395L-E",
+      id: "vid_4"
+   },
+   {
+      name: "Sinal 3", 
+      src: "h#N-klM6y_AtY",
+      id: "vid_5"
+   },
+   {
+      name: "Sinal 3 ALT",
+      src: "h#bk7iVHlRnHU",
+      id: "vid_6"
+   },
+   {
+      name: "Sinal 4",
+      src: "h#zO6zgt-C-vQ",
+      id: "vid_7"
+   },
+   {
+      name: "Sinal 4 ALT",
+      src: "h#osd8Lqn_oVs",
+      id: "vid_8"
+   },
+   {
+      name: "Sinal 5",
+      src: "h#6SO2deNHoCM",
+      id: "vid_9"
+   },
+   {
+      name: "Sinal 5 ALT",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_10"
+   },
+   {
+      name: "Sinal 6",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_12"
+   },
+   {
+      name: "Sinal 6 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_13"
+   },
+   {
+      name: "Sinal 7",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_10"
+   },
+   {
+      name: "Sinal 7 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_11"
+   },
+   {
+      name: "Sinal 8",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_12"
+   },
+   {
+      name: "Sinal 8 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_13"
+   },
+   {
+      name: "Sinal 9",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_14"
+   },
+   {
+      name: "Sinal 9 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_15"
+   },
+   {
+      name: "Sinal 10",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_14"
+   },
+   {
+      name: "Sinal 10 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_15"
+   },
+ 
+   {
+      name: "Sinal 1 720p",
+      src: "https://reidoscanais.com/embed/?id=afazenda15",
+      id: "vid_16"
+   },
+   {
+      name: "Sinal 1 ALT",
+      src: "https://reidoscanais.com/embed/?id=afazenda15",
+      id: "vid_17"
+   },
+   {
+      name: "Sinal 2 720p",
+      src: "h#dOvLae3VlAk",
+      id: "vid_18"
+   },
+   {
+      name: "Sinal 2 ALT",
+      src: "h#li7NA395L-E",
+      id: "vid_19"
+   },
+   {
+      name: "Sinal 3 720p", 
+      src: "h#N-klM6y_AtY",
+      id: "vid_20"
+   },
+   {
+      name: "Sinal 3 ALT",
+      src: "h#bk7iVHlRnHU",
+      id: "vid_21"
+   },
+   {
+      name: "Sinal 4 720p",
+      src: "h#zO6zgt-C-vQ",
+      id: "vid_22"
+   },
+   {
+      name: "Sinal 4 ALT",
+      src: "h#osd8Lqn_oVs",
+      id: "vid_23"
+   },
+   {
+      name: "Sinal 5 720p",
+      src: "h#6SO2deNHoCM",
+      id: "vid_24"
+   },
+   {
+      name: "Sinal 5 ALT",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_25"
+   },
+   {
+      name: "Sinal 6 720p",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_26"
+   },
+   {
+      name: "Sinal 6 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_27"
+   },
+   {
+      name: "Sinal 7 720p",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_28"
+   },
+   {
+      name: "Sinal 7 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_29"
+   },
+   {
+      name: "Sinal 8 720p",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_30"
+   },
+   {
+      name: "Sinal 8 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_31"
+   },
+   {
+      name: "Sinal 9 720p",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_32"
+   },
+   {
+      name: "Sinal 9 ALT",
+      src: "h#Bk3Pe5XPgUI",
+      id: "vid_33"
+   },
+   {
+      name: "Sinal 10 720p",
+      src: "h#hvEa8pnUjCE",
+      id: "vid_34"
+   },
+   {
+      name: "Record TV",
+      src: "http:// tv.wcli.li/record/live.php?ch=1",
+      id: "vid_35"
+   }
+]
